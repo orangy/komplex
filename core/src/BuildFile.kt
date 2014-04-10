4828538c6b6ed4545957d8f4160109249262d4da
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-04-10 13:54:36 +0400

    Add output stream to BuildStream

--- core/src/BuildFile.kt
+++ core/src/BuildFile.kt
@@ -9,6 +9,9 @@ class BuildFile(override val path : Path) : BuildStreamEndPoint {
     override val inputStream: InputStream
         get() = BufferedInputStream(FileInputStream(path.toFile()))
 
+    override val outputStream: OutputStream
+        get() = BufferedOutputStream(FileOutputStream(path.toFile()))
+
     override fun dump(indent: String) {
         println("$indent File ${path}")
     }
--- core/src/BuildStreamEndPoint.kt
+++ core/src/BuildStreamEndPoint.kt
@@ -1,9 +1,27 @@
 package komplex
 
-import java.io.InputStream
+import java.io.*
 import java.nio.file.Path
 
 trait BuildStreamEndPoint : BuildEndPoint {
-    val path : Path
-    val inputStream : InputStream
+    val path: Path
+    val inputStream: InputStream
+    val outputStream: OutputStream
 }
+
+fun MutableList<BuildStreamEndPoint>.addAll(endpoint: BuildEndPoint) {
+    when (endpoint) {
+        is BuildStreamEndPoint -> add(endpoint)
+        is BuildFileSetEndPoint -> addAll(endpoint.findFiles())
+        else -> throw IllegalArgumentException("Unknown endpoint: $endpoint")
+    }
+}
+
+fun List<BuildEndPoint>.getAllStreams(): List<BuildStreamEndPoint> {
+    val streams = arrayListOf<BuildStreamEndPoint>()
+    for (item in this)
+        streams.addAll(item)
+    return streams
+}
+
+
