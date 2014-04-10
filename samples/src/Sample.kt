8804d3c462b9d770847f65840a7bfc51512d52da
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-04-10 19:29:52 +0400

    Cleanup sample

--- samples/src/Sample.kt
+++ samples/src/Sample.kt
@@ -5,54 +5,43 @@ import komplex.jar.*
 import komplex.kotlin.*
 
 fun main(args: Array<String>) = block {
-
     val test = config("test")
     val jar = config("jar")
     val publish = config("publish")
 
-    val testLibs = libraries {
-        // collection of libraries
-        library("junit", "4.11")
-        library("hamcrest-core", "1.3")
-    }
-
-    repository {
-
-    }
-
     shared {
+        version("SNAPSHOT-0.1")
+
+        // shared settings for all projects
         val sources = files("$projectName/src/**")
         val binaries = folder("out/$projectName")
-        val jarFile = folder("artifacts")
+        val jarFile = file("artifacts/$projectName.jar")
 
         build using(tools.kotlin) from sources to binaries
         build(jar, publish) using tools.jar from binaries to jarFile
         build(publish) using tools.publish from jarFile
 
-        // shared settings for all projects
-        version("SNAPSHOT-0.1")
-        depends on testLibs // depends in configuration
-
-        description("Spek " + description) // update description
+        depends on libraries {
+            // collection of libraries
+            library("junit", "4.11")
+            library("hamcrest-core", "1.3")
+        }
 
         building { println("Building $title...") }
         built { println("Done building $title.") }
     }
 
     project("spek") {
-        val core = project("spek-core") // forward declaration
-
-        project("spek-tests", "Tests") {
-            depends on core // reference to project with variable
-            depends on library("mockito-all", "1.9.5", "org.mockito") // inline library dependence
+        val core = project("spek-core", "Spek Core") {
         }
 
-        project("spek-samples", "Samples") {
+        project("spek-samples", "Spek Samples") {
             depends on core // reference to project by name
         }
 
-        project("spek-core", "Core") {
+        project("spek-tests", "Spek Tests") {
+            depends on core // reference to project with variable
+            depends on library("mockito-all", "1.9.5", "org.mockito") // inline library dependence
         }
     }
-
-}.build("publish")
+}.build()
